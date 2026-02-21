32f13f28dda58c14e5b2f9096699a0214c3c1285
SyedMuhammadHaad <haad8j@gmail.com>
2026-02-21 23:11:19 +0200

    message

---
+++ task15.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    system("color 87");
+    cout<<"--------------------------- " <<endl;
+cout<<"    o     ^ ___ ^               " <<endl;
+cout<<"                                " <<endl;
+cout<<"       o   ( oo )\_________     " <<endl;
+cout<<"                                " <<endl;
+cout<<"           (____)\         )\/\ " <<endl;
+cout<<"                 ||------w |    " <<endl;
+cout<<"                                " <<endl;
+cout<<"                 ||       ||    " <<endl;
+cout<<"                                " <<endl;
+cout<<"                                " <<endl;
+}
+   
